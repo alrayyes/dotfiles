d9ef783e03421f2b18b949bb31fe2d80a9280138
Ryan <ryan@andthensome.nl>
2016-11-26 21:40:59 +0100

    removed conky

--- conky/.config/conky/conky-startup.sh
+++
@@ -1,6 +0,0 @@
-sleep 20s
-killall conky
-cd "/home/alrayyes/.conky/Gotham"
-conky -c "/home/alrayyes/.conky/Gotham/Gotham" &
-cd "/home/alrayyes/.conky/TeejeeTech"
-conky -c "/home/alrayyes/.conky/TeejeeTech/CPU Panel (4-core)" &
