7ebabb7145d20f80d1dd8254458ecc038ea6d4da
Ryan <ryan@andthensome.nl>
2017-01-03 20:46:47 +0100

    Added firejail profiles for cmus with cmusfm

---
+++ bin/bin/backup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+REPOSITORY=~/stackdav/thinkpadbackup
+
+# Backup all of /home and /var/www except a few
+# excluded directories
+borg create -v --stats                          \
+    $REPOSITORY::'{hostname}-{now:%Y-%m-%d}'    \
+    /home/alrayyes                              \
+    --exclude '/home/alrayyes/.cache' \
+    --exclude '/home/alrayyes/stackdav' \
+    --exclude '/home/alrayyes/music'            \
+    --exclude '/home/alrayyes/Dropbox'            \
+    --exclude '/home/alrayyes/gPodder/Downloads'
+
+# Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
+# archives of THIS machine. The '{hostname}-' prefix is very important to
+# limit prune's operation to this machine's archives and not apply to
+# other machine's archives also.
+borg prune -v --list $REPOSITORY --prefix '{hostname}-' \
+    --keep-daily=7 --keep-weekly=4 --keep-monthly=6
---
+++ firejail/.config/firejail/cmusfm.profile
@@ -0,0 +1,18 @@
+# cmusfm profile
+noblacklist ${HOME}/.config/cmus
+
+include /etc/firejail/disable-common.inc
+include /etc/firejail/disable-programs.inc
+include /etc/firejail/disable-devel.inc
+include /etc/firejail/disable-passwdmgr.inc
+
+caps.drop all
+netfilter
+nonewprivs
+noroot
+protocol unix,inet,inet6
+seccomp
+
+private-bin cmusfm
+private-etc group
+shell none
