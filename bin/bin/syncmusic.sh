53faba34731cf95a8f7fa75f471f999498528860
Ryan <ryan@andthensome.nl>
2017-04-27 20:22:24 +0200

    added urxvt

--- bin/bin/syncmusic.sh
+++ bin/bin/syncmusic.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rsync -az --delete --progress alrayyes@eniac:/mnt/storage/headphones/ ~/music/headphones
+rsync -az --delete --progress alrayyes@eniac:/mnt/storage/music/ ~/music/headphones
 rsync -az --delete --progress --inplace --size-only ~/music/headphones/ ~/stackdav/music
