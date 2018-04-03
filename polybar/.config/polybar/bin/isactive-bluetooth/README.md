003a1ed7398eb5cf689007bbf4960876c7b3af36
Ryan <ryan@andthensome.nl>
2018-04-03 20:08:57 +0200

    use polybar isntead of bumblebee

---
+++ polybar/.config/polybar/bin/isactive-bluetooth/README.md
@@ -0,0 +1,13 @@
+# Script: isactive-bluetooth
+
+A script that shows if bluetooth is on or off.
+
+
+## Module
+
+```ini
+[module/isactive-bluetooth]
+type = custom/script
+exec = ~/polybar-scripts/isactive-bluetooth.sh
+interval = 10
+```
---
+++ polybar/.config/polybar/bin/updates-arch-combined/updates-arch-combined.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+updates_arch=$(checkupdates | wc -l)
+
+# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
+if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+    updates_aur=0
+fi
+
+updates=$(("$updates_arch" + "$updates_aur"))
+
+if [ "$updates" -gt 0 ]; then
+    echo " $updates"
+else
+    echo ""
+fi
