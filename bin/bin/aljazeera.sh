cf1618ac4978577364df518844dec3bf6075c5f2
Ryan <ryan@andthensome.nl>
2018-04-27 13:38:18 +0200

    detach window while playing

--- bin/bin/aljazeera.sh
+++ bin/bin/aljazeera.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mpv http://aljazeera-eng-apple-live.adaptive.level3.net/apple/aljazeera/english/appleman.m3u8
+mpv -quiet "http://aljazeera-eng-apple-live.adaptive.level3.net/apple/aljazeera/english/appleman.m3u8" > /dev/null & disown
