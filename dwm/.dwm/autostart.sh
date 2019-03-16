ac0b528c95b3a3f250aa7a16b0b1756a681fad5b
Ryan Kes <alrayyes@gmail.com>
2019-03-16 23:25:17 +0100

    use i3lock as screen lock

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-xautolock -time 10 -locker "i3lock -b -f -i ~/.config/lock.png" &
+xautolock -time 10 -locker "i3lock -e -b -f -i ~/.config/lock.png" &
 xbindkeys &
 compton --xrender-sync-fence &
 feh --bg-scale ~/.config/wall.png &
