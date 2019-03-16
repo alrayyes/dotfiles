708fcd19c1d4d8d8e62731c2a16f5fae100ebfcc
Ryan Kes <alrayyes@gmail.com>
2019-03-16 07:13:29 +0100

    removed autostart programs

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -5,16 +5,11 @@ xbindkeys &
 compton --xrender-sync-fence &
 feh --bg-scale ~/.config/wall.png &
 unclutter &
-pasystray &
-nm-applet &
 redshift-gtk &
-syncthing-gtk -m &
 dunst &
 slstatus &
 
-exec st -c weechat -e weechat &
-exec st -c mutt -e neomutt &
-exec st -c ncmpcpp -e ncmpcpp &
+exec st -c tmux -e tmux &
 exec spotify &
 exec gpodder &
 exec firefox &
