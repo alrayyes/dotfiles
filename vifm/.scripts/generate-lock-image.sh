ed746fd9473e182e3866719762442b862cd9a5dc
Ryan Kes <alrayyes@gmail.com>
2019-03-20 15:26:53 +0100

    added generate lock image to vifm

---
+++ vifm/.scripts/generate-lock-image.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+clear
+
+WIDTH=$(xrandr | grep '\*' | awk -F 'x' '{print $1}' | sed 's/ //g')
+OUTPUT_IMAGE="$HOME/.config/lock.png"
+
+echo "Converting ${1} to ${OUTPUT_IMAGE}...."
+convert "${1}" "${OUTPUT_IMAGE}"
+echo "done!"
+echo "Reszing ${OUTPUT_IMAGE} to width ${WIDTH}...."
+convert "${OUTPUT_IMAGE}" -resize "${WIDTH}"x "${OUTPUT_IMAGE}"
+echo "done!"
--- xorg/.scripts/generateLockImage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-WIDTH=$(xrandr | grep '\*' | awk -F 'x' '{print $1}' | sed 's/ //g')
-convert "$1" ~/.config/lock.png
-convert ~/.config/lock.png -resize "${WIDTH}"x ~/.config/lock.png
