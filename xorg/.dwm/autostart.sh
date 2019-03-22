19c426cbecb9d529893bc7e6f281012babcd0d9d
Ryan Kes <alrayyes@gmail.com>
2019-03-22 10:39:59 +0100

    lock laptop on screen close

--- xorg/.dwm/autostart.sh
+++ xorg/.dwm/autostart.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+# lock screen after x minutes and on laptop close lid
 xautolock -time 10 -locker ~/.scripts/lock.sh &
+xss-lock ~/.scripts/lock.sh &
+
 xbindkeys &
 compton --xrender-sync-fence &
 feh --bg-scale ~/.config/wall.png &
