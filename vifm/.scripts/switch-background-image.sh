b3fdfeb16db0ba4b38c930c7380c432631fef29f
Ryan Kes <alrayyes@gmail.com>
2019-03-24 17:18:39 +0100

    added multi monitor wallpaper support

--- vifm/.scripts/switch-background-image.sh
+++ vifm/.scripts/switch-background-image.sh
@@ -2,16 +2,25 @@
 
 clear
 
-WIDTH=$(xrandr | grep '\*' | awk -F 'x' '{print $1}' | sed 's/ //g')
-OUTPUT_IMAGE="$HOME/.cache/wall.png"
+if [ "${2}" = "2" ]
+then
+    OUTPUT_IMAGE="$HOME/.cache/wall2.png"
+    WIDTH=$(xrandr | grep '\*' | sed -n '1p' | awk -F 'x' '{print $2}' | awk -F '_' '{print $1}' | sed 's/ //g')
+    HEIGHT=$(xrandr | grep '\*' | sed -n '1p' | awk -F 'x' '{print $1}' | sed 's/ //g')
+else
+    OUTPUT_IMAGE="$HOME/.cache/wall1.png"
+    # Use tail -n 1 to get last monitor so this works with laptop as well
+    WIDTH=$(xrandr | grep '\*' | tail -n 1 | awk -F 'x' '{print $1}' | sed 's/ //g')
+    HEIGHT=$(xrandr | grep '\*' | tail -n 1 | awk -F 'x' '{print $2}' | awk '{print $1}' | sed 's/ //g')
+fi
 
-echo "Converting ${1} to ${OUTPUT_IMAGE}...."
+echo "Converting ${1} to ${OUTPUT_IMAGE}"
 convert "${1}" "${OUTPUT_IMAGE}"
-echo "done!"
-echo "Reszing ${OUTPUT_IMAGE} to width ${WIDTH}...."
-convert "${OUTPUT_IMAGE}" -resize "${WIDTH}"x "${OUTPUT_IMAGE}"
 
-echo "Setting background to ${OUTPUT_IMAGE}"
-feh --bg-scale ~/.cache/wall.png
+echo "Resizing ${OUTPUT_IMAGE} to ${WIDTH}x${HEIGHT}"
+convert "${OUTPUT_IMAGE}" -resize "${WIDTH}x${HEIGHT}^" -gravity center -extent "${WIDTH}"x"${HEIGHT}" "${OUTPUT_IMAGE}"
+
+echo "Setting background images...."
+feh --bg-scale ~/.cache/wall1.png --bg-scale ~/.cache/wall2.png
 
 echo "done!"
