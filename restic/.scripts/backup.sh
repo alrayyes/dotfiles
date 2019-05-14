311f8cc5eeee4f9550aa624924e60e1e29479e46
Ryan Kes <alrayyes@gmail.com>
2019-05-14 18:45:14 +0200

    updates restic config

--- restic/.scripts/backup.sh
+++ restic/.scripts/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 . /home/alrayyes/.restic
-restic backup --exclude-file=/home/alrayyes/.config/restic/exclude.txt /home/alrayyes
-restic forget -m 6 --prune
+restic backup --exclude-file=/home/alrayyes/.config/restic/exclude.txt /home/alrayyes /etc
+restic forget -m 12 -w 4 -d 14 --prune
