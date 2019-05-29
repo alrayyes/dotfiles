541dcd406c591465b2c3c0db832cbcb8291724e1
Ryan Kes <alrayyes@gmail.com>
2019-05-29 19:53:57 +0200

    added vpn-switch

--- bin/.scripts/vpn-switch.sh
+++ bin/.scripts/vpn-switch.sh
@@ -8,13 +8,13 @@ STATUS="$(nmcli device status | grep -c wg0)"
 
 case $STATUS in
 	1)
-        sudo wg-quick down wg0
+        sudo wg-quick down wg0 | notify-send
         sudo wg-quick down wg1
-        notify-send  "down"
 		;;
 	*)
         sudo wg-quick up wg0
         sudo wg-quick up wg1
-        notify-send  "up"
 		;;
   esac
+
+notify-send 'adsf'
