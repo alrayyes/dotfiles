0f2b0139e362e3bf287e71d960adef5caf3f7d99
Ryan Kes <ryan@ryankes.eu>
2019-12-02 15:50:13 +0100

    Added projectile paths

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -2,33 +2,33 @@
 
 ;; Place your private configuration here
 ;; General settings
-(setq doom-font (font-spec :family "FuraCode Nerd Font Mono" :size 12))
-(setq doom-theme 'doom-molokai)
+(setq doom-font (font-spec :family "FuraCode Nerd Font Mono" :size 12)
+      doom-theme 'doom-molokai)
+
+;; projectile
+(setq projectile-project-search-path '("~/devel/personal/" "~/devel/andthensome/" "~/Documents/" "~"))
 
 ;; irc
 (add-hook 'circe-channel-mode-hook 'enable-lui-autopaste)
-(use-package! circe
-  :config
-  (setq circe-network-options
-        `(
-          ("znc-freenode"
-           :host "irc.higherlearning.eu"
-           :port "5000"
-           :tls t
-           :user "l0rd/freenode"
-           :server-buffer-name "⇄ Freenode (ZNC)"
-           :pass (lambda (&rest _) (+pass-get-secret "controlpanel/irc.higherlearning.eu"))
-           :channels ("#emacs"))
-          ("znc-snoonet"
-           :host "irc.higherlearning.eu"
-           :port "5000"
-           :tls t
-           :user "l0rd/snoonet"
-           :server-buffer-name "⇄ Snoonet (ZNC)"
-           :pass (lambda (&rest _) (+pass-get-secret "controlpanel/irc.higherlearning.eu"))
-           :channels ("#islam"))
-          ))
-  )
+(setq circe-network-options
+      `(
+        ("znc-freenode"
+         :host "irc.higherlearning.eu"
+         :port "5000"
+         :tls t
+         :user "l0rd/freenode"
+         :server-buffer-name "⇄ Freenode (ZNC)"
+         :pass (lambda (&rest _) (+pass-get-secret "controlpanel/irc.higherlearning.eu"))
+         :channels ("#emacs"))
+        ("znc-snoonet"
+         :host "irc.higherlearning.eu"
+         :port "5000"
+         :tls t
+         :user "l0rd/snoonet"
+         :server-buffer-name "⇄ Snoonet (ZNC)"
+         :pass (lambda (&rest _) (+pass-get-secret "controlpanel/irc.higherlearning.eu"))
+         :channels ("#islam"))
+        ))
 
 ;; temporarily fix bug
 (fset 'battery-update #'ignore)
