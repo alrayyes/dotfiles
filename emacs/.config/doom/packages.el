64bf663cafec24aa2624f23ca55f6acf6ae1d79b
Ryan Kes <>
2020-11-22 11:51:02 +0100

    refactor(emacs): removed packages already included by doom

--- emacs/.config/doom/packages.el
+++ emacs/.config/doom/packages.el
@@ -5,12 +5,11 @@
 ;; (package! some-package)
 ;; (package! another-package :recipe (:host github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
+
 (package! elfeed-protocol)
 (package! elfeed-goodies)
 (package! org-protocol-capture-html :recipe (:host github :repo "alphapapa/org-protocol-capture-html"))
-(package! docker-compose-mode)
 (package! nginx-mode)
-(package! fish-mode)
 (package! salt-mode)
 (package! fountain-mode)
 (package! org-ref)
